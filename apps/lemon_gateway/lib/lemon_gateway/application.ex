d2b5371aad3c77bcab00d4f2f5e914f7b439f709
z80 <z80@ophy.xyz>
2026-02-04 22:58:31 -0500

    Revert "feat: Connect Web UI to LemonGateway via direct WebSocket"
    This reverts commit cbf45433324c6601cb82927848b1282c1882cf74.

--- apps/lemon_gateway/lib/lemon_gateway/application.ex
+++ apps/lemon_gateway/lib/lemon_gateway/application.ex
@@ -7,8 +7,6 @@ defmodule LemonGateway.Application do
 
   @impl true
   def start(_type, _args) do
-    web_port = Application.get_env(:lemon_gateway, :web_port, 3939)
-
     base_children = [
       LemonGateway.Config,
       LemonGateway.EngineRegistry,
@@ -21,19 +19,14 @@ defmodule LemonGateway.Application do
       LemonGateway.Scheduler,
       LemonGateway.Store
     ]
-    
-    # Start the Cowboy web server
-    web_children = [
-      {Plug.Cowboy, scheme: :http, plug: LemonGateway.Web.Router, options: [port: web_port, dispatch: dispatch()]}
-    ]
 
     # Only start legacy TransportSupervisor if lemon_channels is NOT active
     # to avoid duplicate ingestion from both systems
     children =
       if lemon_channels_active?() do
-        base_children ++ web_children
+        base_children
       else
-        base_children ++ [{LemonGateway.TransportSupervisor, []}] ++ web_children
+        base_children ++ [{LemonGateway.TransportSupervisor, []}]
       end
 
     # See https://hexdocs.pm/elixir/Supervisor.html
@@ -42,19 +35,16 @@ defmodule LemonGateway.Application do
     Supervisor.start_link(children, opts)
   end
 
-  defp dispatch do
-    [
-      {:_, [
-        {"/ws", LemonGateway.Web.SocketHandler, []},
-        {:_, Plug.Cowboy.Handler, {LemonGateway.Web.Router, []}}
-      ]}
-    ]
-  end
-
   # Check if lemon_channels application is running and has adapters enabled
   defp lemon_channels_active? do
-    Application.get_env(:lemon_gateway, :use_lemon_channels, false) &&
-      :lemon_channels in Enum.map(Application.started_applications(), fn {app, _, _} -> app end)
+    # Check if the lemon_channels application is started
+    case Application.ensure_started(:lemon_channels) do
+      :ok -> true
+      {:error, {:already_started, :lemon_channels}} -> true
+      _ ->
+        # Also check if it's in the started applications
+        :lemon_channels in Enum.map(Application.started_applications(), fn {app, _, _} -> app end)
+    end
   rescue
     # If lemon_channels doesn't exist, fall back to legacy
     _ -> false
