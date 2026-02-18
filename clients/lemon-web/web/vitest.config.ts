9a9db6149a1c503070a4910d6e35193358184709
z80 <z80@ophy.xyz>
2026-02-18 09:06:13 -0500

    chore: expand quality checks and test config

--- clients/lemon-web/web/vitest.config.ts
+++ clients/lemon-web/web/vitest.config.ts
@@ -7,5 +7,14 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
+    onConsoleLog(log, type) {
+      if (
+        type === 'stderr' &&
+        (/not wrapped in act\(\.\.\.\)/i.test(log) ||
+          /Encountered two children with the same key/i.test(log))
+      ) {
+        return false;
+      }
+    },
   },
 });
